b0025e2083e730077646b637e8a0982399bbdc77
Faron-Kupudlo <daniel.faron.1993@gmail.com>
2020-04-26 16:08:56 +0200

    poprawione models, template dodawania wpisu

--- moje_piwo/dziennik/models.py
+++ moje_piwo/dziennik/models.py
@@ -1,18 +1,27 @@
 from django.db import models
+from django.urls import reverse
 from django.core.validators import MaxValueValidator, MinValueValidator
-
-
 
 class wpis(models.Model):
     nazwa_piwa = models.CharField(default='' , max_length=50)
-    surowiec_1 = models.CharField(default='',max_length=50)
-    surowiec_1_ilosc = models.FloatField('ilość', default=0, validators=[MaxValueValidator(1000000), MinValueValidator(0)])
+    surowiec_1 = models.CharField('', default='',max_length=50, blank=True)
+    surowiec_1_ilosc = models.FloatField('', default=0, validators=[MaxValueValidator(1000000), MinValueValidator(0)], blank=True)
     class jednostka (models.TextChoices):
         l = 'l', ('litr')
         g = 'g', ('gram')
         kg = 'kg', ('kilogram')
 
-    surowiec_1_jednostka = models.CharField('jednostka', max_length=10, choices=jednostka.choices, default=jednostka.l)
+    surowiec_1_jednostka = models.CharField('', max_length=10, choices=jednostka.choices, default=jednostka.l, blank=True)
+    surowiec_2 = models.CharField('', default='', max_length=50, blank=True)
+    surowiec_2_ilosc = models.FloatField('', default=0,
+                                         validators=[MaxValueValidator(1000000), MinValueValidator(0)], blank=True)
+    surowiec_2_jednostka = models.CharField('', max_length=10, choices=jednostka.choices, default=jednostka.l,
+                                            blank=True)
+    surowiec_3 = models.CharField('', default='', max_length=50, blank=True)
+    surowiec_3_ilosc = models.FloatField('', default=0,
+                                         validators=[MaxValueValidator(1000000), MinValueValidator(0)], blank=True)
+    surowiec_3_jednostka = models.CharField('', max_length=10, choices=jednostka.choices, default=jednostka.l,
+                                            blank=True)
     drożdże = models.CharField(max_length=100)
 
     class fermentacja (models.TextChoices):
@@ -25,4 +34,9 @@ class wpis(models.Model):
     temperatura_D_D = models.FloatField('temperatura dodania drożdży', default=19, validators=[MaxValueValidator(1000000), MinValueValidator(0)])
     startowa_wartość_BLG = models.FloatField(default=0, validators=[MaxValueValidator(1000000), MinValueValidator(0)])
     końcowa_wartość_BLG = models.FloatField(default=0, validators=[MaxValueValidator(1000000), MinValueValidator(0)])
-    # przebieg fermentacji burzliwej
+
+    def __str__(self):
+        return self.nazwa_piwa
+
+    def get_absolute_url(self):
+        return reverse('index')
--- moje_piwo/dziennik/views.py
+++ moje_piwo/dziennik/views.py
@@ -11,10 +11,16 @@ from .models import wpis
 
 class wpisCreateView(LoginRequiredMixin, CreateView):
     model = wpis
-    fields = ['nazwa_piwa','surowce_1','drożdże','fermentacja',
-              'objetosc','temperatura_D_D','startowa_wartosc_BLG',
-              'koncowa_wartosc_BLG']
+    fields = ['nazwa_piwa',
+              'surowiec_1', 'surowiec_1_ilosc', 'surowiec_1_jednostka',
+              'surowiec_2', 'surowiec_2_ilosc', 'surowiec_2_jednostka',
+              'surowiec_3', 'surowiec_3_ilosc', 'surowiec_3_jednostka',
+              'drożdże','fermentacja',
+              'objetosc','temperatura_D_D','startowa_wartość_BLG',
+              'końcowa_wartość_BLG']
     template_name = 'createView.html'
+
+
 
 def index(request):
     return render(request, 'index.html')
